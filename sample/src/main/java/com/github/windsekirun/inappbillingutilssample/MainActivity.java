8d43528f8c603b118989237a55b061e4db9c68aa
WindSekirun <h.mina.moe@gmail.com>
2017-01-11 17:58:49 +0900

    Impl purchase part on SampleApplication

--- sample/src/main/java/com/github/windsekirun/inappbillingutilssample/MainActivity.java
+++ sample/src/main/java/com/github/windsekirun/inappbillingutilssample/MainActivity.java
@@ -3,6 +3,9 @@ package com.github.windsekirun.inappbillingutilssample;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.github.windsekirun.inappbillingtest.InAppBillingUtils;
 import com.github.windsekirun.inappbillingtest.model.Sku;
@@ -16,10 +19,18 @@ public class MainActivity extends AppCompatActivity implements InAppBillingUtils
     String item2Sku = "item_2";
     String item3Sku = "item_3";
 
+    Button btn1;
+    Button btn2;
+    Button btn3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = (Button) findViewById(R.id.button);
+        btn2 = (Button) findViewById(R.id.button2);
+        btn3 = (Button) findViewById(R.id.button3);
+
         utils = new InAppBillingUtils(this, "", this);
 
         utils.initInAppBilling();
@@ -30,7 +41,30 @@ public class MainActivity extends AppCompatActivity implements InAppBillingUtils
         items.add(item3Sku);
 
         ArrayList<Sku> availablePackage = utils.getAvailableInappPackage(items);
+        for (Sku sku : availablePackage) {
+            Log.d(sku.getProductId(), sku.getPrice() + ": " + sku.getTitle() + " : " + sku.getType());
+        }
 
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                utils.purchase(item1Sku);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                utils.purchase(item2Sku);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                utils.purchase(item3Sku);
+            }
+        });
     }
 
     @Override
